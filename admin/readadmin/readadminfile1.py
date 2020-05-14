1443443eb911d0eb1d1a803f5dfbdf1db71f6cd5
TLRKiliann <philogenie@protonmail.com>
2020-05-14 21:59:10 +0200

    admin files read ok

--- admin/readadmin/readadminfile1.py
+++ admin/readadmin/readadminfile1.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-#!-*-encoding:Utf-8-*-
+# -*-encoding:Utf-8-*-
 
 
 from tkinter import *
@@ -14,18 +14,19 @@ def importationFile(fichier):
 
 fen=Tk()
 fen.title("Admininstrative Statement")
-fen.configure(background='gray17')
+fen.configure(background='grey17')
 
 labelo=Label(fen, text="Admininstrative Statement",
-    font='Arial 18 bold', fg='turquoise', bg='gray17')
+    font='Arial 18 bold', fg='turquoise', bg='grey17')
 labelo.pack(pady=10)
 
 textBox=Text(fen, height=15, width=60, font=18)
 textBox.pack(padx=30, pady=30)
 
-buttonClose=Button(fen, text="Quitter", fg='cyan', bg='gray30',
-    activebackground='dark turquoise', activeforeground='navy', command=quit)
-buttonClose.pack(side='right')
+buttonClose=Button(fen, text="Quit", width=10, bd=3,
+    fg='white', bg='navy', activebackground='dark turquoise',
+    highlightbackground='grey17', command=quit)
+buttonClose.pack(side='right', padx=10, pady=10)
 
 importationFile('./admin/readadmin/fileAdmin1.txt')
 
