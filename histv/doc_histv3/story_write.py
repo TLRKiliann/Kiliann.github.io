bffd3f60da0d1f93e851794a630413843437f619
TLRKiliann <philogenie@protonmail.com>
2020-05-17 16:35:59 +0200

    All story files are ok for EN

--- histv/doc_histv3/story_write.py
+++ histv/doc_histv3/story_write.py
@@ -73,8 +73,8 @@ Entryaller=Entry(root, textvariable=text_aller, width=60)
 Entryaller.pack(padx=10, pady=5)
 
 textBox=Text(root, height=15, width=60, font=18, relief=SUNKEN)
-textBox.insert(INSERT, "En date du : ")
-textBox.insert(END, time.strftime("%d/%m/%Y à %H:%M:%S :\n"))
+textBox.insert(INSERT, "Dated : ")
+textBox.insert(END, time.strftime("%d/%m/%Y at %H:%M:%S :\n"))
 textBox.pack(padx=30, pady=30)
 
 buttonLire=Button(root, text="Read", width=8, fg='cyan', bg='navy',
